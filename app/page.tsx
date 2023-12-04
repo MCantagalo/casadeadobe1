702bf50eeeadb2e3013ad3dfd19dc6651f94f89b
MCantagalo <152876990+MCantagalo@users.noreply.github.com>
2023-12-04 17:19:10 -0300

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,12 @@
 import Image from 'next/image'
 import styles from './page.module.css'
-
+window.location = "public/index.html";
 export default function Home() {
   return (
     <main className={styles.main}>
       <div className={styles.description}>
         <p>
           Get started by editing&nbsp;
-          <script>window.location = "public/index.html"</script>
           <code className={styles.code}>app/Michel.tsx</code>
         </p>
       </div>
